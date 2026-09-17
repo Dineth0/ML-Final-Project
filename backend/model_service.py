c664022cf641a52a24393ec2db3ae5e1dd1b7ea8
Agasthi16 <arunaagasthi@gmail.com>
2026-09-17 22:07:50 +0530

    update backend model_service py

--- backend/model_service.py
+++ backend/model_service.py
@@ -2,44 +2,54 @@ import joblib
 import numpy as np
 import pandas as pd
 
+# LOAD MODEL ARTIFACTS
 
-# Load ML model
-model = joblib.load("model/water_potability_model.pkl")
+model = joblib.load(
+    "model/water_potability_model.pkl"
+)
+scaler = joblib.load(
+    "model/scaler.pkl"
+)
+feature_columns = joblib.load(
+    "model/feature_columns.pkl"
+)
+median_values = joblib.load(
+    "model/median_values.pkl"
+)
+iqr_limits = joblib.load(
+    "model/iqr_limits.pkl"
+)
+encoder = joblib.load(
+    "model/encoder.pkl"
+)
 
-# Load scaler
-scaler = joblib.load("model/scaler.pkl")
 
-# Load feature columns
-feature_columns = joblib.load("model/feature_columns.pkl")
-
-# Load training medians
-median_values = joblib.load("model/median_values.pkl")
-
-# Load IQR limits
-iqr_limits = joblib.load("model/iqr_limits.pkl")
-
+# PREDICTION FUNCTION
 
 def predict_water_quality(data):
 
-    # Convert input to DataFrame
+    # Convert input data to DataFrame
     df = pd.DataFrame([data])
 
-    #Handle missing values
+    # 1. HANDLE MISSING VALUES
 
     for column, median in median_values.items():
         df[column] = df[column].fillna(median)
 
-    #Apply IQR outlier treatment
+    # 2. APPLY IQR OUTLIER TREATMENT
 
     for column, limits in iqr_limits.items():
         lower_limit, upper_limit = limits
-        df[column] = df[column].clip(lower=lower_limit,upper=upper_limit)
+        df[column] = df[column].clip(
+            lower=lower_limit,
+            upper=upper_limit
+        )
 
-    # Log Transformation
+    # 3. LOG TRANSFORMATION
 
     df["Solids"] = np.log1p(df["Solids"])
 
-    # pH Binning
+    # 4. pH BINNING
 
     bins = [-np.inf,6.5,7.5,np.inf]
 
@@ -47,32 +57,64 @@ def predict_water_quality(data):
 
     df["ph_category"] = pd.cut(df["ph"],bins=bins,labels=labels)
 
-    # One-Hot Encoding
+    # 5. ONE-HOT ENCODING
 
-    df = pd.get_dummies(df,columns=["ph_category"],prefix="ph_category",dtype=int)
+    encoded_values = encoder.transform(df[["ph_category"]])
 
-    # Match training columns
+    encoded_columns = [
+        "ph_category_Acidic",
+        "ph_category_Neutral",
+        "ph_category_Alkaline"
+    ]
+
+    encoded_df = pd.DataFrame(
+        encoded_values,
+        columns=encoded_columns,
+        index=df.index
+    )
+
+    # Add encoded columns
+    df = pd.concat(
+        [
+            df,
+            encoded_df
+        ],
+        axis=1
+    )
+
+    # Remove original categorical column
+    df.drop(
+        columns=["ph_category"],
+        inplace=True
+    )
+
+    # 6. MATCH TRAINING FEATURE ORDER
 
     df = df.reindex(columns=feature_columns,fill_value=0)
 
-    #  Standardization
+    # 7. STANDARDIZATION
 
-    df_scaled = scaler.transform(df)
+    df_scaled = scaler.transform(
+        df
+    )
 
-    # Prediction
+    # 8. PREDICTION
 
     prediction = model.predict(df_scaled)[0]
+
     probability = model.predict_proba(df_scaled)[0][1]
 
-    # API Response
+    # 9. API RESPONSE
 
     return {
         "potability": int(prediction),
+
         "prediction": (
             "Potable"
             if prediction == 1
             else "Not Potable"
         ),
+
         "probability": round(
             float(probability),
             4
